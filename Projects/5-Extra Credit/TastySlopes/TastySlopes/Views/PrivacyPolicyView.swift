d5b7838b0f0a7c6cc46eb92f68545b17a8ba7701
Lea Marolt Sonnenschein <leyamars@gmail.com>
2019-11-29 13:57:31 +0100

    Updating the hierarchy for PrivacyPolicyView

---
+++ Projects/5-Extra Credit/TastySlopes/TastySlopes/Views/PrivacyPolicyView.swift
@@ -0,0 +1,25 @@
+//
+//  PrivacyPolicyView.swift
+//  TastySlopes
+//
+//  Created by Lea Marolt Sonnenschein on 26/11/2019.
+//  Copyright © 2019 Lea Marolt Sonnenschein. All rights reserved.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+  var body: some View {
+    VStack {
+      Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+      Spacer()
+    }
+    .padding()
+  }
+}
+
+struct PrivacyPolicyView_Previews: PreviewProvider {
+  static var previews: some View {
+    PrivacyPolicyView()
+  }
+}
